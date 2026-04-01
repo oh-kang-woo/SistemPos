687ced51c296518f98bc24653de30d0ac8505885
syawil66 <syawilrahmat@gmail.com>
2026-04-01 12:10:37 +0700

    migration & model untuk kategori dan produk

---
+++ database/migrations/2026_04_01_050245_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('id_produk');
+            $table->foreignId('kategori_id')->constrained('categories', 'id_kategori')->onDelete('cascade');
+            $table->string('kode_produk')->unique();
+            $table->string('nama_produk');
+            $table->string('satuan')->default('pcs');
+            $table->integer('harga_beli');
+            $table->integer('harga_jual');
+            $table->integer('jumlah_stok');
+            $table->integer('min_stok')->default(10);
+            $table->enum('status', ['aktif', 'nonaktif'])->default('aktif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
